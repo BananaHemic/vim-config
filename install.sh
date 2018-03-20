3bdf99dc0f606c3670586e86242fa0027b5d0daa
bananahemic <bananahemic@users.noreply.github.com>
2018-03-20 22:13:40 +0000

    Added sudo check

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #~/bin/sh
-sudo apt-get install build-essential cmake python-dev python3-dev
+if [ $(id -u) = 0 ]; then
+    echo "Please do NOT run this as sudo"
+    exit 1
+fi
+sudo apt-get install build-essential cmake python-dev python3-dev python3-pip
 ln -s "$(pwd)/vimrc" ~/.vimrc
 ln -s "$(pwd)/vsvimrc" ~/_vsvimrc
 ln -s "$(pwd)/vimcommon" ~/_vimcommon
@@ -7,8 +11,8 @@ ln -s "$(pwd)/vimcommon" ~/_vimcommon
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 vim +PluginInstall +qall
 #Make sure we have node install for JS
-type node >/dev/null 2>&1 || {echo "Will install node"; curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - sudo apt-get install -y nodejs;}
+type node >/dev/null 2>&1 || { echo "Will install node"; curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash - sudo apt-get install -y nodejs;}
 #Install YouCompleteMe
 python ~/.vim/bundle/YouCompleteMe/install.py --tern-completer --clang-completer
 #Install syntastic checker
-pip install pylint
+pip3 install pylint
